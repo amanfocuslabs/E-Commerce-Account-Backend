43d06b98fe0b505bd622459d491a4b4dff3b64a0
aman <amanget4@gmail.com>
2019-07-15 10:25:12 -0500

    Project Starter

---
+++ src/main/java/com/ea/ecommerceaccountbackend/ECommerceAccountBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ea.ecommerceaccountbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAccountBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceAccountBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ea/ecommerceaccountbackend/ECommerceAccountBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ea.ecommerceaccountbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ECommerceAccountBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

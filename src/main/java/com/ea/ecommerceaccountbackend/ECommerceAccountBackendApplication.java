0766d2ab850ee10746e6bf7c03dfa067e15a4311
Wuletaw Adane <wule2000@gmail.com>
2019-07-15 22:02:12 -0500

    Latest Account

--- src/main/java/com/ea/ecommerceaccountbackend/ECommerceAccountBackendApplication.java
+++ src/main/java/com/ea/ecommerceaccountbackend/ECommerceAccountBackendApplication.java
@@ -2,8 +2,10 @@ package com.ea.ecommerceaccountbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ECommerceAccountBackendApplication {
 
 	public static void main(String[] args) {
